a2bce56fb8d68cffc2fb4e031238f189311f9e95
soulteary <soulteary@gmail.com>
2023-05-12 23:12:38 +0800

    chore: refactor the background

--- extension/background.js
+++ extension/background.js
@@ -1,8 +1,139 @@
-const DEFAULT_REPORT_SERVER = "http://localhost:8080/exchange";
+const DEFAULT_REPORT_SERVER = "http://localhost:8080/Exchange";
 let ConnReady = false;
 let Mutex = false;
 
-async function exchange(url = "", data = {}) {
+/*! --------------------------------
+// Simple Execution Process
+-------------------------------- */
+
+AppInit()
+  .then(function (config) {
+    if (!config) {
+      Exchange(DEFAULT_REPORT_SERVER, "fail to fetch the config");
+      return;
+    }
+
+    const { job, server, report, base, min, max } = config;
+    let conn = CreateWebsocketConn(server, report);
+
+    let connChecker = setInterval(function () {
+      console.log(ConnReady);
+      if (ConnReady) {
+        clearInterval(connChecker);
+        BootStrap(conn, job, base, min, max);
+        return;
+      }
+    }, 500);
+  })
+  .catch(function (err) {
+    console.log(err);
+    Exchange(DEFAULT_REPORT_SERVER, "fail to fetch the config");
+  });
+
+/*! --------------------------------
+// example task
+-------------------------------- */
+
+// Continuously create tasks
+function watchdog(conn, job, baseTime, minTime, maxTime) {
+  return function () {
+    if (Mutex) return;
+    // delay execution after
+    var delay = getRandomDelay(baseTime, minTime, maxTime) * 1000;
+    Mutex = true;
+    conn.send("创建延时任务:" + delay);
+    setTimeout(function () {
+      Mutex = false;
+      ReloadPage(job, function (data) {
+        conn.send("页面重载执行结果" + data);
+      });
+    }, delay);
+  };
+}
+
+// Continuously collect data
+function stats(conn) {
+  return function () {
+    conn.send("stats ok");
+  };
+}
+
+/*! --------------------------------
+// browser, extension, server... comminicates
+-------------------------------- */
+
+/**
+ * Init the extension app after fetching the config
+ * @returns
+ */
+async function AppInit() {
+  const config = await Exchange("http://localhost:8080/config");
+  return config && config.code == 200 ? config.data : null;
+}
+
+function BootStrap(conn, job, base, min, max) {
+  // reload the page, auto renew the session
+  let w = watchdog(conn, job, base, min, max);
+  setInterval(w, 1000);
+
+  // eg, stats report
+  let s = stats(conn);
+  setInterval(s, 1000);
+
+  // eg, inject code, scroll the page to bottom...
+  setInterval(function () {
+    Execute(job, scrollPageToBottom);
+  }, 1000);
+}
+
+/*! --------------------------------
+// chrome functions
+-------------------------------- */
+
+/**
+ * Execute the given command
+ * @param {string} job
+ * @param {function} fn
+ */
+function Execute(job, fn) {
+  chrome.tabs.query({ url: job }, function (tabs) {
+    tabs.forEach(function (tab) {
+      chrome.scripting.executeScript({ target: { tabId: tab.id, allFrames: true }, func: fn }).then((injectionResults) => {
+        for (const frameResult of injectionResults) {
+          const { frameId, result } = frameResult;
+          console.log(`Frame #${frameId} 执行结果:`, result);
+        }
+      });
+    });
+  });
+}
+
+/**
+ * Reload the chrome tab which matches the job url
+ * @param {string} server
+ * @param {function} callback
+ */
+function ReloadPage(server, callback) {
+  chrome.tabs.query({ url: server }, function (tabs) {
+    tabs.forEach(function (tab) {
+      chrome.tabs.reload(tab.id);
+      callback("重载完毕");
+    });
+  });
+}
+
+/*! --------------------------------
+// network connection
+-------------------------------- */
+
+/**
+ * Data Exchange with the server
+ * ref: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ * @param {string} url
+ * @param {object} data
+ * @returns
+ */
+async function Exchange(url = "", data = {}) {
   const response = await fetch(url, {
     method: "POST",
     mode: "cors",
@@ -15,16 +146,17 @@ async function exchange(url = "", data = {}) {
   return response.json();
 }
 
-async function AppInit() {
-  const config = await exchange("http://localhost:8080/config");
-  return config && config.code == 200 ? config.data : null;
-}
-
-function GetWebsocketConn(server, report) {
+/**
+ * Create a connection to a remote server
+ * @param {string} server
+ * @param {string} report
+ * @returns
+ */
+function CreateWebsocketConn(server, report) {
   var conn = new WebSocket(server);
   conn.onclose = function (evt) {
     console.log(evt);
-    exchange(report, "Connection closed.");
+    Exchange(report, "Connection closed.");
     ConnReady = false;
   };
   conn.onmessage = function (evt) {
@@ -39,8 +171,26 @@ function GetWebsocketConn(server, report) {
   return conn;
 }
 
-// scroll page to bottom
-async function scrollToBottom() {
+/*! --------------------------------
+// utils, injected functions...
+-------------------------------- */
+
+/**
+ * Make interaction times somewhat random
+ * @param {int} base
+ * @param {int} min
+ * @param {int} max
+ * @returns
+ */
+function getRandomDelay(base, min, max) {
+  return base + Math.floor(Math.random() * min) + max;
+}
+
+/**
+ * Scroll the page to bottom
+ * @returns void
+ */
+async function scrollPageToBottom() {
   return new Promise((resolve) => {
     let totalHeight = 0;
     let distance = 500;
@@ -55,92 +205,3 @@ async function scrollToBottom() {
     }, 100);
   });
 }
-
-// Make interaction times somewhat random
-function getRandomDelay(base, min, max) {
-  return base + Math.floor(Math.random() * min) + max;
-}
-
-// Continuously create tasks
-function watchdog(conn, job, baseTime, minTime, maxTime) {
-  return function () {
-    if (Mutex) return;
-    // delay execution after
-    var delay = getRandomDelay(baseTime, minTime, maxTime) * 1000;
-    Mutex = true;
-    conn.send("创建延时任务:" + delay);
-    setTimeout(function () {
-      Mutex = false;
-      reloadPage(job, function (data) {
-        conn.send("页面重载执行结果" + data);
-      });
-    }, delay);
-  };
-}
-
-// Continuously collect data
-function stats(conn) {
-  return function () {
-    conn.send("stats ok");
-  };
-}
-
-function reloadPage(server, callback) {
-  chrome.tabs.query({ url: server }, function (tabs) {
-    tabs.forEach(function (tab) {
-      chrome.tabs.reload(tab.id);
-      callback("true");
-    });
-  });
-}
-
-function inject(job, fn) {
-  chrome.tabs.query({ url: job }, function (tabs) {
-    tabs.forEach(function (tab) {
-      chrome.scripting.executeScript({ target: { tabId: tab.id, allFrames: true }, func: fn }).then((injectionResults) => {
-        for (const frameResult of injectionResults) {
-          const { frameId, result } = frameResult;
-          console.log(`Frame ${frameId} result:`, result);
-        }
-      });
-    });
-  });
-}
-
-function BootStrap(conn, job, base, min, max) {
-  // reload the page, auto renew the session
-  let w = watchdog(conn, job, base, min, max);
-  setInterval(w, 1000);
-
-  // eg, stats report
-  let s = stats(conn);
-  setInterval(s, 1000);
-
-  setInterval(function () {
-    inject(job, scrollToBottom);
-  }, 1000);
-}
-
-AppInit()
-  .then(function (config) {
-    if (!config) {
-      exchange(DEFAULT_REPORT_SERVER, "fail to fetch the config");
-      return;
-    }
-
-    const { job, server, report, base, min, max } = config;
-    let conn = GetWebsocketConn(server, report);
-
-    let connChecker = setInterval(function () {
-      console.log(ConnReady);
-      if (ConnReady) {
-        clearInterval(connChecker);
-        BootStrap(conn, job, base, min, max);
-        return;
-      }
-    }, 500);
-  })
-  .catch(function (err) {
-    console.log(err);
-    exchange(DEFAULT_REPORT_SERVER, "fail to fetch the config");
-  });
